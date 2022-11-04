0f30b1b849aa847ca5ecca7838c527eee5161373
Nandlal <shahnandlal49@gmail.com>
2022-11-04 20:28:16 +0530

    done

--- day-1/Assignment3/index.ts
+++ day-1/Assignment3/index.ts
@@ -1,2 +1,16 @@
 const naya:Array<number>=[1,2,3,4,]
 
+interface User {
+    type: 'user';
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+
+interface Admin {
+    type: 'admin';
+    name: string;
+    age: number;
+    role: string;
+}
